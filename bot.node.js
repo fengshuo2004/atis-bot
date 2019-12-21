9a618226c965881f38315ee261b418c4e7464070
Feng Shuo <fengshuo2004@163.com>
2019-12-21 09:05:42 +0800

    Fixing undefined field crashing the bot

--- bot.node.js
+++ bot.node.js
@@ -6,10 +6,10 @@ const airports = require('./airports.json');
 const client = new Discord.Client();
 var player, voice;
 
-function sendEmbed(ctx,color,title,description){
+function sendEmbed(ctx, color, title, description) {
     ctx.channel.send(new Discord.RichEmbed()
-        .setColor(["#FC440F","#B4E33D","#1F01B9"][color])
-        .setAuthor("ATIS Bot","https://i.imgur.com/uyHC7bZ.png")
+        .setColor(["#FC440F", "#B4E33D", "#1F01B9"][color])
+        .setAuthor("ATIS Bot", "https://i.imgur.com/uyHC7bZ.png")
         .setTitle(title)
         .setDescription(description)
         .setFooter("Requested by user " + ctx.member.user.tag)
@@ -34,7 +34,7 @@ function getMetar(icao) { // THIS IS ASYNC!
 
 // command functions definitions
 function help(ctx) {
-    sendEmbed(ctx,2,":bulb: **Help** for using this ATIS bot:",
+    sendEmbed(ctx, 2, ":bulb: **Help** for using this ATIS bot:",
         `• Type \`${PF}atis <ICAO code>\` to listen to the ATIS live frequency at your specified airport\n` +
         `• Type \`${PF}metar <ICAO code>\` to receive the lastest METAR weather report of that airport\n` +
         `• Type \`${PF}atis stop\` to stop the current ATIS frequency stream`);
@@ -49,7 +49,7 @@ function atis(ctx, args) {
             player.end();
             break;
         case undefined: // nothing entered
-            sendEmbed(ctx, 2,":mag: Please provide an airport ICAO code","Currently, only the following airports are available:\n" +
+            sendEmbed(ctx, 2, ":mag: Please provide an airport ICAO code", "Currently, only the following airports are available:\n" +
                 `\`${Object.keys(airports).join(" ")}\`\nType \`${PF}help\` for more information`);
             break;
         default: // normally
@@ -57,24 +57,24 @@ function atis(ctx, args) {
                 if (res.length == 3) { // if airport has TWO atis frequencies
                     switch (which) { // what's the second argument?
                         case "arr": // arrival
-                            sendEmbed(ctx,1,`:loud_sound: :airplane_arriving: Now streaming the arrival ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
+                            sendEmbed(ctx, 1, `:loud_sound: :airplane_arriving: Now streaming the arrival ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
                                 "You may now connect to the voice channel to listen to it\n" +
                                 `Type \`${PF}atis stop\` to stop the audio stream`);
                             player = voice.playArbitraryInput(res[1]);
                             break;
                         case "dep": // departure
-                            sendEmbed(ctx,1,`:loud_sound: :airplane_departure: Now streaming the departure ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
+                            sendEmbed(ctx, 1, `:loud_sound: :airplane_departure: Now streaming the departure ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
                                 "You may now connect to the voice channel to listen to it\n" +
                                 `Type \`${PF}atis stop\` to stop the audio stream`);
                             player = voice.playArbitraryInput(res[2]);
                             break;
                         default: // didn't specify or some other bullshit user typed in
-                            sendEmbed(ctx,2,":twisted_rightwards_arrows: The airport you chose has **two** ATIS frequencies", "one for arrival and one for departure\n" +
+                            sendEmbed(ctx, 2, ":twisted_rightwards_arrows: The airport you chose has **two** ATIS frequencies", "one for arrival and one for departure\n" +
                                 `• To listen to the **arrival** frequency, type \`${PF}atis ${airport} arr\`\n` +
                                 `• To listen to the **departure** frequency, type \`${PF}atis ${airport} dep\``);
                     }
                 } else {
-                    sendEmbed(ctx,1,`:loud_sound: :airplane: Now streaming the ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
+                    sendEmbed(ctx, 1, `:loud_sound: :airplane: Now streaming the ATIS frequency of **${airport.toUpperCase()} - ${res[0]}**`,
                         "You may now connect to the voice channel to listen to it\n" +
                         `Type \`${PF}atis stop\` to stop the audio stream`);
                     player = voice.playArbitraryInput(res[1]);
@@ -85,24 +85,52 @@ function atis(ctx, args) {
     }
 }
 
-function metar(ctx,args) {
-    getMetar(args[0]).then( // Request is successful
-        function(metar){
-            if (metar.results == 0) { // no result
-                sendEmbed(ctx, 0,":no_entry_sign: No result","Make sure you are entering a valid ICAO code of an existing airport.");
+function metar(ctx, args) {
+    getMetar(args[0]).then(
+        function (metar) { // handle a successful request
+            if (metar.results == 0) { // no result :-(
+                sendEmbed(ctx, 0, ":no_entry_sign: No result", "Make sure you are entering a valid ICAO code of an existing airport.");
             } else {
-                sendEmbed(ctx, 1, `Current METAR weather report of \`${args[0]}\``,`:dash: **Wind**: ${metar.data[0].wind.speed_kts} Knots @ ${metar.data[0].wind.degrees} Degrees
-:thermometer: **Temperature**: ${metar.data[0].temperature.celsius}\u2103 / ${metar.data[0].temperature.fahrenheit}\u2109
-:droplet: **Dewpoint**: ${metar.data[0].dewpoint.celsius}\u2103 / ${metar.data[0].dewpoint.fahrenheit}\u2109
-:cloud_rain: **Humidity**: ${metar.data[0].humidity.percent}%
-:chart_with_upwards_trend: **Barometer**: ${metar.data[0].barometer.hg}Hg
-:eye: **Visibility**: ${metar.data[0].visibility.miles} Miles`);
+                let i = metar.data[0]; // Many fields are optional, so we must expect its non-existence
+                let content = "";
+                try { content += `\`${i.flight_category}\` Premitted\n\n` } catch{ };
+                try {
+                    content += `:dash: **Wind**: ${i.wind.speed_kts} Knots @ ${i.wind.degrees} Degrees${(i.wind.gust_kts) ? `, Gust ${i.wind.gust_kts} Knots` : ""}\n`;
+                } catch{ };
+                try { content += `:thermometer: **Temperature**: ${i.temperature.celsius}\u2103 / ${i.temperature.fahrenheit}\u2109\n` } catch{ };
+                try { content += `:droplet: **Dewpoint**: ${i.dewpoint.celsius}\u2103 / ${i.dewpoint.fahrenheit}\u2109\n` } catch{ };
+                try { content += `:umbrella: **Humidity**: ${i.humidity.percent}%\n` } catch{ };
+                try { content += `:chart_with_upwards_trend: **Barometer**: ${i.barometer.hg}Hg\n` } catch{ };
+                try { content += `:cloud_rain: Currently **raining** ${i.rain.inches} Inches\n` } catch{ };
+                try { content += `:snowflake: Currently **snowing** ${i.snow.inches} Inches\n` } catch{ };
+                if (i.clouds && i.clouds.length > 0) { // is array and not empty
+                    content += "\n:cloud: **Clouds**\n";
+                    i.clouds.forEach(
+                        function (element, index) {
+                            content += `Layer ${index + 1} - ${element.text}${(element.base_feet_agl) ? ` @ ${element.base_feet_agl}ft` : ""}\n`
+                        }
+                    );
+                }
+                if (i.conditions && i.conditions.length > 0) { // is array and not empty
+                    content += "\n:warning: **Conditions**\n";
+                    i.conditions.forEach(
+                        function (element) {
+                            content += `${element.text}\n`
+                        }
+                    );
+                }
+                content += `\n:scroll: **Raw entry**: \`${i.raw_text}\`\n*These data were observed at ${i.observed}*`;
+                sendEmbed(ctx, 1, `Latest METAR report of **${i.icao} - ${i.station.name}**`, content);
             }
         }
     )
-    .catch(
-        console.error
-    );
+        .catch(
+            function (error) {
+                console.error("\u2717   Promise rejected, request failed due to: " + error);
+                sendEmbed(ctx, 0, ":dizzy_face: Unable to fetch data", "The HTTP request to CheckWX's API has failed, this problem is logged." +
+                    `\n**Internal Error: ${error.toString()}**`);
+            }
+        );
 }
 
 // Maps all command word to functions
@@ -121,17 +149,17 @@ function main(message) { // onMessage event handler
     }
     const args = message.content.slice(PF.length).trim().split(/ +/g); // parse arguments into a list
     const command = args.shift().toLowerCase(); // parse out the command words
-    if (commands[command]){
-        try{
-        (commands[command])(message, args); // execute command
-        } catch (e){
-            sendEmbed(message,0,":dizzy_face: Something bad happened deep inside of me!",
-            "This problem is automatically logged and will be dealt with by the developers shortly" +
-            `\n**Internal Error: ${e.toString()}**`);
+    if (commands[command]) {
+        try {
+            (commands[command])(message, args); // execute command
+        } catch (e) {
+            sendEmbed(message, 0, ":dizzy_face: Something bad happened deep inside of me!",
+                "This problem is automatically logged and will be dealt with by the developers shortly" +
+                `\n**Internal Error: ${e.toString()}**`);
             console.log("\u2717   Exception occured while executing command: " + e);
         }
     } else {
-        sendEmbed(message,0,`:no_entry_sign: There is no such command as \`${command}\`!`,
+        sendEmbed(message, 0, `:no_entry_sign: There is no such command as \`${command}\`!`,
             `Type \`${PF}help\` to get a list of available commands for this bot`);
     }
 }
